f67e3f206cbeeb075231b3a559ab9c5df6fbdef2
Caroline Zhang <caroline.fzhang@gmail.com>
2025-10-16 23:49:58 +1100

    update database connection

--- backend/Vanlife/Vanlife/settings.py
+++ backend/Vanlife/Vanlife/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'Vanlife.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'vanlife'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
-        'OPTIONS': {
-            'sslmode': 'require',
-        },
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': 'postgres',
+        'USER': os.environ.get('DB_USER'),
+        'PORT': '5432',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
